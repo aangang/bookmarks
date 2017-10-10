cab40e89b02c4d5ed0b855028cd766a5202b3ea6
aangang <1253228348@qq.com>
2017-10-10 19:13:14 +0800

    add bookmark 2 add bookmark to browser by self

--- bookmarks/images/templates/bookmarklet_launcher.js
+++ bookmarks/images/templates/bookmarklet_launcher.js
@@ -5,3 +5,8 @@
         document.body.appendChild(document.createElement('script')).src='http://127.0.0.1:8000/static/js/bookmarklet.js?r='+Math.floor(Math.random()*99999999999999999999); 
     } 
 })();
+
+
+//because some browser can not support so,
+//handly add this js code to bookmark of browser
+//javascript:(function(){if(window.myBookmarklet!==undefined){myBookmarklet(); } else{document.body.appendChild(document.createElement('script')).src='http://127.0.0.1:8000/static/js/bookmarklet.js?r='+Math.floor(Math.random()*99999999999999999999); } })();
