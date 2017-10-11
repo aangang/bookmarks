8e9fef9cfd0ed236c09c26d0a8afe60536fb2c10
gang.an <1253228348@qq.com>
2017-10-11 21:03:21 +0800

    image view 3 detail view image

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -24,8 +24,10 @@ SECRET_KEY = '^+==%m^!(#mm@sp6jj)h@7!8v$3ei^ef%gr#3u0(eivea(1a2_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = []
+#DEBUG = False
+#ALLOWED_HOSTS = ['127.0.0.1', '8000']
+
 
 
 # Application definition
